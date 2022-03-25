66ec1e87ded76d227d5d88ccaddb658f66fe68f8
RainyWithHikari <101868227+RainyWithHikari@users.noreply.github.com>
2022-03-25 11:43:37 +0800

    Add files via upload

---
+++ src/utils/defaultSorts.js
@@ -0,0 +1,19 @@
+const defaultSorts = [
+    {
+        id:'1',
+        sortName:'sort1',
+        sortDescription:'sort1'
+    },
+    {
+        id:'2',
+        sortName:'sort2',
+        sortDescription:'sort2'
+    },
+    {
+        id:'3',
+        sortName:'sort3',
+        sortDescription:'sort3'
+    }
+]
+
+export default defaultSorts
